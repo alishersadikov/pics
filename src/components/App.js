f3c8a5b6063e491b8aae384e8207aecd68efe322
Alisher Sadikov <alishersadikov@gmail.com>
2019-12-30 11:03:44 -0600

    Axios to fetch images

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,18 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
+
+require("dotenv").config();
 
 class App extends React.Component {
   onSearchSubmit(term) {
     console.log(term);
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization: "Client-ID"
+      }
+    });
   }
 
   render() {
